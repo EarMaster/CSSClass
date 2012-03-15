0143d55e5439e3fc0dc7c85789ed9ea1511ce277
unknown <Nico Wiedemann@YellowSubmarine.(none)>
2012-03-15 11:21:22 +0100

    added notrim version (see README for details)

---
+++ CSSClass.notrim.js
@@ -0,0 +1,22 @@
+var p = Element.prototype;
+if(!p.hasClass)
+	p.hasClass = function(c) {
+		return new RegExp('\\b' + c + '\\b', 'g').test(this.className);
+	};
+if(!p.addClass)
+	p.addClass = function(c) {
+		if(!this.hasClass(c))
+			this.className = (this.className + ' ' + c);
+		return this;
+	};
+if(!p.removeClass)
+	p.removeClass = function(c) {
+		c = c.split(' ');
+		for(var i = 0; i < c.length; i++)
+		this.className = this.className.replace(new RegExp('\\b' + c + '\\b', 'g'), '');
+		return this;
+	};
+if(!p.toggleClass)
+	p.toggleClass = function(c) {
+		return !this.hasClass(c) ? this.addClass(c) : this.removeClass(c);
+	};
