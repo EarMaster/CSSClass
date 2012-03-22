f61f80650b8a6071116ab881603785cc722b819e
Nico Wiedemann <nico.wiedemann@gmx.de>
2012-03-22 16:50:32 +0100

    Added splitting in addClass

--- CSSClass.notrim.js
+++ CSSClass.notrim.js
@@ -1,22 +1,24 @@
 var p = Element.prototype;
 if(!p.hasClass)
 	p.hasClass = function(c) {
-		return new RegExp('\\b' + c + '\\b', 'g').test(this.className);
+		return new RegExp('\\b'+c+'\\b', 'g').test(this.className);
 	};
 if(!p.addClass)
 	p.addClass = function(c) {
-		if(!this.hasClass(c))
-			this.className = (this.className + ' ' + c);
+		c = c.split(' ');
+		for(var i=0; i<c.length; i++)
+			if(!this.hasClass(c[i]))
+				this.className = (this.className+' '+c[i]);
 		return this;
 	};
 if(!p.removeClass)
 	p.removeClass = function(c) {
 		c = c.split(' ');
-		for(var i = 0; i < c.length; i++)
-		this.className = this.className.replace(new RegExp('\\b' + c + '\\b', 'g'), '');
+		for(var i=0; i<c.length; i++)
+			this.className = this.className.replace(new RegExp('\\b'+c+'\\b', 'g'), '');
 		return this;
 	};
 if(!p.toggleClass)
 	p.toggleClass = function(c) {
-		return !this.hasClass(c) ? this.addClass(c) : this.removeClass(c);
+		return !this.hasClass(c)?this.addClass(c):this.removeClass(c);
 	};
